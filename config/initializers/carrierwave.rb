b1b3406ae7a0c5aad75cc1cf3124a6aa998c163b
aryngazydastan <aryngazy.dastan@gmail.com>
2015-09-16 23:51:04 +0600

    Update carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,14 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = { 
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AKIAJYQ5Z43ZVT322FIA'],
-      :aws_secret_access_key  => ENV['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'],
-  }
-  config.cache_dir = "#{Rails.root}/tmp/uploads" 
-  config.fog_directory  = ENV['repositoryiitu']
-end
+if Rails.env.production?
+        CarrierWave.configure do |config|
+        config.root = Rails.root.join('tmp')
+        config.cache_dir = 'carrierwave'
+
+        config.fog_credentials = {
+          # Configuration for Amazon S3
+          :provider               => 'AWS',                        # change var's name
+          :aws_access_key_id      => ENV['AKIAJYQ5Z43ZVT322FIA'],         # change var's name
+          :aws_secret_access_key  => ENV['CWvLX5RNfVVh0XwIdCGc2KzJTXyOIvt4xLsGpZv']
+        }
+        config.fog_directory     =  ENV['repositoryiitu']
+      end
+    end
